63506312efd5f42a924b61d6dbd3b977fa0a6996
eduardo moran <eduardo.moran.davalos@gmail.com>
2019-02-08 10:13:09 +0600

    Updates

---
+++ precache-manifest.e12d0a7fc39927e12cdb9f227af26782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b86ac8d95cc828f3bf01a84fac9ee6f6",
+    "url": "/Clicky-game/static/media/cartographer.b86ac8d9.png"
+  },
+  {
+    "revision": "0bb66f981b0e8665f72f",
+    "url": "/Clicky-game/static/js/runtime~main.0bb66f98.js"
+  },
+  {
+    "revision": "0b8d99265a4686c82752",
+    "url": "/Clicky-game/static/js/main.0b8d9926.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/Clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "0b8d99265a4686c82752",
+    "url": "/Clicky-game/static/css/main.85759e97.chunk.css"
+  },
+  {
+    "revision": "4c914520c50864e450d292dca6354780",
+    "url": "/Clicky-game/index.html"
+  }
+];
